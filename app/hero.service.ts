51b5f03b9a0834d17d9b136f3ea44b21aa5550ad
Gareth Fleming <gareth@joocy.com>
2016-01-25 22:35:12 +0000

    initial commit

---
+++ app/hero.service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from 'angular2/core';
+import {HEROES} from './mock-heroes';
+import {Hero} from './hero'
+
+@Injectable()
+export class HeroService {
+    getHeroes() {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(() => resolve(HEROES), 2000));
+    }
+}
